3afcc780745b04b06821914a64a75984549f9765
Sebastiaan Nijs <basie14105@gmail.com>
2023-11-16 23:44:26 +0100

    Added front end
    Made a start to the front end, none of the buttons/textfields do anything just the styling

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,18 @@
+@extends('layout.layout')
+
+
+@section('content')
+    <h1>Terms</h1>
+    <div>
+        orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
+        type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of
+        Lorem Ipsum
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script>
+    </body>
+@endsection
